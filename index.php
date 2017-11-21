ec6e09aa04d711d76beeb6069401139168c79599
rodrigots83 <santos.rodrigo.teixeira@gmail.com>
2017-11-21 16:46:57 -0200

    teste

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 echo "Teste de inclusão de arquivo no GITHUB";
 
-echo "<br><strong>vamos fazer uma nova inclusão!</strong>"
+echo "<br><strong>vamos fazer uma nova inclusão!</strong>";
+
+echo "<br><strong>vamos fazer uma alteração fora das alterações comuns!!!</strong>";
 
 ?>
